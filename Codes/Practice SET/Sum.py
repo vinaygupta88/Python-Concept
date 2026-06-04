0b04a87d09ed300fa510ef7d993901897c83f18a
Vinay Kumar <vinayku73094@gmail.com>
2026-06-04 17:42:38 +0530

    Task 1

---
+++ Codes/Practice SET/Sum.py
@@ -0,0 +1,10 @@
+# Write a Program to input 2 number & print their sum, difference and product
+num1=int(input("Enter First number: "))
+num2=int(input("Enter Second number: "))
+
+
+print("Sum : ", num1+num2)
+print("Difference : ", num1-num2)
+print("Product : ", num1*num2)
+print("Remainder : ", num1%num2)
+print("\nAverage of two float number: ",(float(num1)+float(num2))/2)
---
+++ Codes/Practice SET/sqaureArea.py
@@ -0,0 +1,3 @@
+side = float(input("Enter the side of Square: "))
+Area = side * side
+print("Area of sqaure: ",Area)
